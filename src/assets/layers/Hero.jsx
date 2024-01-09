11ce99857b71dbb9e8ca822d8a0c60fc1569ecee
itsannaw <emikot321@gmail.com>
2024-01-09 22:43:51 +0300

    init: create hero

---
+++ src/assets/layers/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section>
+      <div className="flex mt-5">
+        <div className="flex mt-40">
+          <img src="/src/assets/images/HeroBG.png" alt="#" />
+        </div>
+        <div className="flex justify-between items-center gap-10">
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
