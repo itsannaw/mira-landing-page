6efc8a3fd1f5a38c401d8bb747124d05c5f7049b
itsannaw <emikot321@gmail.com>
2024-01-18 22:27:35 +0300

    feat: add cards

---
+++ src/assets/layers/HowItWorks.jsx
@@ -0,0 +1,47 @@
+import Card from "../../components/Card";
+import Hashtag from "../../components/Hashtag";
+import { HOW_IT_WORKS_CARDS } from "../../const";
+
+const HowItWorks = () => {
+  return (
+    <section className="flex mt-[120px] flex-col gap-10">
+      <div className="flex gap-[508px]">
+        <div className="">
+          <Hashtag># Фичи и контент</Hashtag>
+        </div>
+        <div className="flex flex-col max-w-[480px] gap-[25px]">
+          <span className=" text-black text-[40px] leading-[44px] tracking-[0.5px]">
+            Как работает <br /> приложение?
+          </span>
+          <span className="text-black text-lg leading-[22px] tracking-[0.7px]">
+            При создании Миры команда разработки опиралась на 3 главных «кита»,
+            которые делают приложение таким полезным
+          </span>
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-between">
+          <Card type={HOW_IT_WORKS_CARDS.PLUS}>
+            Вы фиксируете эмоции и мысли, которые с ними связаны, в любое
+            удобное для вас время. «Мира» запоминает эти данные и бережно хранит
+            их на вашем устройстве.
+          </Card>
+          <Card type={HOW_IT_WORKS_CARDS.CHART}>
+            На основе сделанных записей Мира формирует статистику, которая
+            поможет посмотреть на жизненную ситуацию со стороны: обратить
+            внимание на аспекты, которые вызывают больше всего позитивных или,
+            напротив, отрицтельных эмоций.
+          </Card>
+          <Card type={HOW_IT_WORKS_CARDS.PRAXIS}>
+            Мира подбирает психотерапевтическую технику, опираясь на ваше
+            состояние. Упражнения внимательно отобраны специалистом, проходятся
+            за короткое время и имеют доказанную эффективность.
+          </Card>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
+
+export default HowItWorks;
---
+++ src/components/Card.jsx
@@ -0,0 +1,56 @@
+import { useMemo } from "react";
+import { HOW_IT_WORKS_CARDS } from "../const";
+
+const CARDS_MAPPER = {
+  [HOW_IT_WORKS_CARDS.PLUS]: {
+    src: "/src/assets/images/works/Plus.svg",
+    alt: "plus",
+    button_text: "Записи (чек-ины)",
+    number: "01",
+  },
+  [HOW_IT_WORKS_CARDS.CHART]: {
+    src: "/src/assets/images/works/Chart.svg",
+    alt: "chart",
+    button_text: "Статистика",
+    number: "02",
+  },
+  [HOW_IT_WORKS_CARDS.PRAXIS]: {
+    src: "/src/assets/images/works/Praxis.svg",
+    alt: "praxis",
+    button_text: "Упражнения",
+    number: "03",
+  },
+};
+
+const Card = (props) => {
+  const cards = useMemo(() => {
+    return CARDS_MAPPER[props.type];
+  }, [props.type]);
+
+  return (
+    <div className="flex flex-col gap-5 bg-[#F6F5F8] rounded-[20px] max-w-[427px] py-8 px-9 shadow-card">
+      <div className="flex gap-3">
+        <button className="flex items-center max-w-[287px] w-full text-white bg-violet rounded-3xl py-[10px] px-3 border-2 border-[#CFD0DA]">
+          <span className="text-[18px] leading-[22px] tracking-[0.7px]">
+            {cards.button_text}
+          </span>
+        </button>
+        <div className="flex justify-center items-center w-[56px] h-[56px] bg-light_orange rounded-full">
+          <img className="w-9 h-9" src={cards.src} alt={cards.alt} />
+        </div>
+      </div>
+      <div className="flex flex-col  justify-between gap-5 h-[195px]">
+        <img src="/src/assets/images/works/border.png" alt="" />
+        <span className="h-[150px] text-base leading-[22px] tracking-[0.1px]">
+          {props.children}
+        </span>
+        <img src="/src/assets/images/works/border.png" alt="" />
+      </div>
+      <span className="flex justify-center text-[200px] text-violet font-medium leading-[170px] opacity-50">
+        {cards.number}
+      </span>
+    </div>
+  );
+};
+
+export default Card;
