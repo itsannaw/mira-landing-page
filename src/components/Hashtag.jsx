12c10b0954744df9502c66c35d360def74d759bc
Anna Galkevich <emikot321@gmail.com>
2024-01-20 18:54:05 +0300

    Revert "Team"

--- src/components/Hashtag.jsx
+++
@@ -1,9 +0,0 @@
-const Hashtag = (props) => {
-  return (
-    <button className="text-violet text-[14px] px-[14px] py-3 border-violet rounded-2xl border-2 pointer-none">
-      {props.children}
-    </button>
-  );
-};
-
-export default Hashtag;
