b06b0a6251212bd9593b90c07db35ac838bb1a95
itsannaw <emikot321@gmail.com>
2024-01-26 22:52:51 +0300

    fix: hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import About from "./public/layers/About";
-import Footer from "./public/layers/Footer";
-import Header from "./public/layers/Header";
-import Hero from "./public/layers/Hero";
-import HowItWorks from "./public/layers/HowItWorks";
-import Team from "./public/layers/Team";
+import About from "./assets/layers/About";
+import Footer from "./assets/layers/Footer";
+import Header from "./assets/layers/Header";
+import Hero from "./assets/layers/Hero";
+import HowItWorks from "./assets/layers/HowItWorks";
+import Team from "./assets/layers/Team";
 
 function App() {
   return (
