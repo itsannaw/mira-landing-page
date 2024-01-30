8c52f8109b3e0d6d7929bac3e05bf2d3610c7e52
itsannaw <emikot321@gmail.com>
2024-01-30 23:57:57 +0300

    feat: continued adaptive layout

--- src/components/DetailedCard.jsx
+++ src/components/DetailedCard.jsx
@@ -7,7 +7,7 @@ const CARDS_MAPPER = {
     bg_color: "bg-dark_violet",
     text_color: "text-white",
     size_phone:
-      "w-[457px] h-[523px] max-lg:w-[380px] max-lg:h-[342px] max-lg:left-[70px] left-[110px] max-lg:top-[80px]",
+      "xl:w-[457px] xl:h-[523px] w-[290px] h-[260px] left-[70px] xl:left-[110px] top-[80px]",
     phone: "/images/works/Mockups1.png",
     small_phone: "/images/works/Mockups1m.png",
     alt: "phone_plus",
@@ -26,14 +26,15 @@ const CARDS_MAPPER = {
     text3:
       "Записи хранятся на главной странице приложения, их можно открывать, редактировать, удалять и отправлять своему специалисту.",
     number: "/images/works/1.svg",
-    location_number: "right-0 lg:right-20",
+    location_number:
+      "right-0 max-xl:top-[-10px] max-xl:h-[300px] max-xl:w-[116px] xl:right-20 ",
   },
   [HOW_IT_WORKS_CARDS.CHART]: {
     id: "card2",
     bg_color: "bg-[#EFECFC]",
     text_color: "text-black",
     size_phone:
-      "max-lg:w-[400px] max-lg:h-[351px] max-lg:bottom-[100px] w-[500px] h-[585px] left-[69px]",
+      "max-xl:w-[290px] max-xl:h-[260px] max-xl:bottom-[100px] w-[500px] h-[585px] left-[69px]",
     phone: "/images/works/Mockups2.png",
     small_phone: "/images/works/Mockups2m.png",
     alt: "phone_chart",
@@ -44,14 +45,15 @@ const CARDS_MAPPER = {
     text3:
       "Таким образом, зайдя в статистику, можно сразу понять, какие сферы жизни влияют на вас больше всего, какие из них дают больше отрицательных эмоций, а какие – положительных.",
     number: "/images/works/2.svg",
-    location_number: "right-0 bottom-[0.5px]",
+    location_number:
+      "right-0 max-xl:top-5 xl:bottom-[0.5px] max-xl:h-[300px] max-xl:w-[186px]",
   },
   [HOW_IT_WORKS_CARDS.PRAXIS]: {
     id: "card3",
     bg_color: "bg-violet",
     text_color: "text-white",
     size_phone:
-      "max-lg:w-[420px] max-lg:h-[420px] max-lg:bottom-[50px] max-xl:left-[40px] w-[565px] h-[595px] left-[9px] bottom-[-5px] ",
+      "max-xl:w-[290px] max-xl:h-[260px] max-xl:bottom-[50px] max-xl:left-[40px] w-[565px] h-[595px] left-[9px] bottom-[-5px] ",
     phone: "/images/works/Mockups3.png",
     small_phone: "/images/works/Mockups3m.png",
     alt: "phone_praxis",
@@ -62,7 +64,7 @@ const CARDS_MAPPER = {
     text3:
       "Техники, которые Мира подобрала для пользователя, можно найти в соответствующем разделе меню и пройти в любое время. Список упражнений будет пополняться по мере взаимодействия с приложением.",
     number: "/images/works/3.svg",
-    location_number: "right-0",
+    location_number: "right-0 max-xl:top-5 max-xl:h-[300px] max-xl:w-[208px]",
   },
 };
 
@@ -74,7 +76,7 @@ const DetailedCard = (props) => {
   return (
     <div className="card" id={card.id}>
       <div
-        className={`relative overflow-hidden w-full lg:h-[625px] p-[20px] lg:pt-10  ${card.bg_color} rounded-[20px] z-10 duration-500`}
+        className={`relative overflow-hidden w-full xl:h-[625px] p-[20px] lg:pt-10  ${card.bg_color} rounded-[20px] z-10 duration-500`}
       >
         <img
           className={`absolute max-xl:hidden ${card.size_phone}`}
@@ -82,7 +84,7 @@ const DetailedCard = (props) => {
           alt="phone"
         />
         <img
-          className={`absolute ${card.location_number} max-lg:w-[116px] max-lg:h-[300px]`}
+          className={`absolute ${card.location_number} max-xl:w-[116px] max-xl:h-[300px]`}
           src={card.number}
           alt="card-number"
         />
@@ -94,16 +96,16 @@ const DetailedCard = (props) => {
             {card.text1}
           </span>
 
-          <span className="max-xl:text-base text-lg leading-[22px] tracking-[0.7px]">
+          <span className="text-sm md:text-base xl:text-lg leading-[16px] font-medium xl:font-normal xl:leading-[22px] tracking-[0.5px] xl:tracking-[0.7px]">
             {card.text2()}
           </span>
           <img src={`/images/works/${card.border}`} alt="#" />
-          <span className="max-xl:text-xl text-[28px] leading-[38px] tracking-[0.7px]">
+          <span className="text-[14px] md:text-xl xl:text-[28px] leading-[18px] xl:leading-[38px] tracking-[0.1px] xl:tracking-[0.7px] font-medium xl:font-normal">
             {card.text3}
           </span>
           <img src={`/images/works/${card.border}`} alt="#" />
           <img
-            className={`xl:hidden ${card.size_phone}`}
+            className={`xl:hidden flex justify-center items-center mx-auto ${card.size_phone}`}
             src={card.small_phone}
             alt="phone"
           />
