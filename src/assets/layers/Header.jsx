5962ca3bfd785ea5b434c69232d3eef3413ca4ad
itsannaw <emikot321@gmail.com>
2024-01-31 20:20:34 +0300

    feat: continued adaptive layout

--- src/assets/layers/Header.jsx
+++ src/assets/layers/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
   const links = LINKS_META.map((link) => (
     <a
       key={link.text}
-      className="text-sm lg:font-normal font-bold cursor-pointer"
+      className="text-sm lg:font-normal font-bold max-lg:leading-[16px] max-lg:tracking-[0.5px] cursor-pointer"
       onClick={(e) => scrollToType(e, link.scrollType)}
     >
       {link.text}
@@ -30,7 +30,7 @@ const Header = () => {
 
   return (
     <section className="max-w-[1440px] mx-auto w-full">
-      <div className="mira-header-before flex flex-col lg:flex-row fixed w-[calc(min(100%,1440px)-30px)] lg:w-[calc(min(100%,1440px)-80px)] bg-white z-20 left-[calc(50%-min(660px,calc((100%/2)-15px)))] lg:left-[calc(50%-min(680px,calc((100%/2)-40px)))] top-0 justify-between items-center py-3 border-b-2 border-[#CFD0DA]">
+      <div className="mira-header-before flex flex-col lg:flex-row fixed w-[calc(min(100%,1440px)-30px)] lg:w-[calc(min(100%,1440px)-80px)] bg-white z-20 left-[calc(50%-min(660px,calc((100%/2)-15px)))] lg:left-[calc(50%-min(680px,calc((100%/2)-40px)))] top-0 justify-between min-[450px]:items-center py-3 border-b-2 border-[#CFD0DA]">
         <div className="flex justify-between w-full z-10">
           <div className="w-[114px] h-[52px] lg:w-[131px] lg:h-[60px]">
             <a href="">
@@ -52,7 +52,9 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <div className="flex gap-4 lg:hidden z-10">{links}</div>
+        <div className="flex gap-4 mt-[10px] justify-between lg:hidden z-10">
+          {links}
+        </div>
       </div>
     </section>
   );
