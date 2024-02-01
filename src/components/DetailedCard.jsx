2d88b27542558b5ed76ed639545d79b36551b85e
itsannaw <emikot321@gmail.com>
2024-02-01 23:07:47 +0300

    fix: bags in layer HowItWorks

--- src/components/DetailedCard.jsx
+++ src/components/DetailedCard.jsx
@@ -7,7 +7,7 @@ const CARDS_MAPPER = {
     bg_color: "bg-dark_violet",
     text_color: "text-white",
     size_phone:
-      "xl:w-[457px] xl:h-[523px] md:w-[360px] md:h-[330px] w-[290px] h-[260px] left-[70px] xl:left-[110px] top-[40px]",
+      "max-lg:md:max-w-[360px] xl:w-[457px] xl:h-[523px] md:w-[360px] md:h-[330px] w-[290px] h-[260px] left-[70px] xl:left-[110px] top-[40px]",
     phone: "/images/works/Mockups1.png",
     small_phone: "/images/works/Mockups1m.png",
     alt: "phone_plus",
@@ -33,7 +33,7 @@ const CARDS_MAPPER = {
     bg_color: "bg-[#EFECFC]",
     text_color: "text-black",
     size_phone:
-      "w-[290px] h-[260px] md:w-[360px] md:h-[330px] xl:w-[500px] xl:h-[585px] left-[69px]",
+      "max-lg:md:max-w-[360px] w-[290px] h-[260px] md:w-[360px] md:h-[330px] xl:w-[500px] xl:h-[585px] left-[69px]",
     phone: "/images/works/Mockups2.png",
     small_phone: "/images/works/Mockups2m.png",
     alt: "phone_chart",
@@ -52,7 +52,7 @@ const CARDS_MAPPER = {
     bg_color: "bg-violet",
     text_color: "text-white",
     size_phone:
-      "w-[300px] h-[270px] left-[40px] md:w-[360px] md:h-[330px] xl:w-[565px] xl:h-[595px] left-[9px] bottom-[-5px] ",
+      "max-lg:md:max-w-[360px] w-[300px] h-[270px] left-[40px] md:w-[360px] md:h-[330px] xl:w-[565px] xl:h-[595px] left-[9px] bottom-[-5px] ",
     phone: "/images/works/Mockups3.png",
     small_phone: "/images/works/Mockups3m.png",
     alt: "phone_praxis",
@@ -75,7 +75,7 @@ const DetailedCard = (props) => {
   return (
     <div className="card" id={card.id}>
       <div
-        className={`relative overflow-hidden w-full xl:h-[625px] sm:h-[590px] min-[767px]:h-[780px] lg:h-[440px] p-[20px] md:pt-10  ${card.bg_color} rounded-[20px] z-10 duration-500`}
+        className={`relative overflow-hidden w-full xl:h-[625px] sm:h-[590px] md:h-[440px] lg:h-[440px] p-[20px] md:pt-10  ${card.bg_color} rounded-[20px] z-10 duration-500`}
       >
         <img
           className={`absolute max-xl:hidden ${card.size_phone}`}
@@ -89,18 +89,18 @@ const DetailedCard = (props) => {
         />
 
         <div
-          className={`flex flex-col min-[767px]:flex-row-reverse max-xl:mx-auto max-xl:items-center justify-center xl:relative xl:left-[calc(50%-10px)] gap-5 xl:max-w-[500px] lg:w-full ${card.text_color}`}
+          className={`flex flex-col md:flex-row-reverse max-xl:mx-auto max-xl:items-center justify-center xl:relative xl:left-[calc(50%-10px)] gap-5 xl:max-w-[500px] lg:w-full ${card.text_color}`}
         >
           <div className="flex flex-col w-full max-w-[480px] gap-5">
             <span className="text-2xl xl:text-[40px] max-xl:text-3xl leading-[44px] tracking-[0.5px]">
               {card.text1}
             </span>
 
-            <span className="text-sm md:text-base xl:text-lg leading-[16px] font-bold xl:font-normal xl:leading-[22px] tracking-[0.5px] xl:tracking-[0.7px]">
+            <span className="text-sm md:text-[14px] xl:text-lg leading-[16px] font-bold xl:font-normal xl:leading-[22px] tracking-[0.5px] xl:tracking-[0.7px]">
               {card.text2()}
             </span>
             <img src={`/images/works/${card.border}`} alt="#" />
-            <span className="text-[14px] md:text-lg xl:text-[28px] leading-[18px] xl:leading-[38px] tracking-[0.1px] xl:tracking-[0.7px] font-medium xl:font-normal">
+            <span className="text-[14px] md:text-base xl:text-[28px] leading-[18px] xl:leading-[38px] tracking-[0.1px] xl:tracking-[0.7px] font-medium xl:font-normal">
               {card.text3}
             </span>
 
